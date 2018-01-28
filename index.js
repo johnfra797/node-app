d5b20a238102d3daaf2da3014c69b3977725b451
Johnfra797 <johnfra797@hotmail.com>
2018-01-27 22:07:35 -0500

    example cors

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hey, I\'m a Node.js app!')
-})
+app.use(cors())
 
+var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.get('/', (req, res) => {
+    res.send('mmm!')
+})
+app.get('/hello', cors(corsOptions), function (req, res, next) {
+    res.send('Hey, I\'m a Node.js app!')
+    
+  })
 app.listen(4500, () => {
     console.log('Server is up on 4500')
 })
